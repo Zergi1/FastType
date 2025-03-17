0ce69fa078526e4433a56425c5b7e5a588c81bcd
Набойщиков.Алексей <.@TERM1>
2025-03-17 11:32:46 +0300

    ...

--- AppData/TypingService.cs
+++ AppData/TypingService.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Controls;
 
 namespace FastType.AppData
 {
-    internal class TypingService
+    public class TypingService
     {
+        // Создаем поля для хранения и использования в рамках класса
+        private Grid _keyboardDrid;
+        private TextBox _typingTextBox;
+        private TextBlock _typingTextBlock;
+        // Создаем конструктор класса для ПРИЕМА элемента управления из интерфейса (TypingTutorPage.xaml)
+
+        public TypingService(Grid keyboardDrid, TextBox typingTextBox, TextBlock typingTextBlock)
+        {
+            _keyboardDrid = keyboardDrid;
+            _typingTextBox = typingTextBox;
+            _typingTextBlock = typingTextBlock;
+            _typingTextBox.PreviewKeyDown += _typingTextBox_PreviewKeyDown;
+            _typingTextBox.PreviewKeyUp += _typingTextBox_PreviewKeyUp;
+        }
+
+        private void _typingTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void _typingTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            _typingTextBox.Clear();
+            _typingTextBox.Text = e.Key.ToString();
+
+        }
     }
 }
