f8514b3921d240b30d8f616b8775abd27da876f2
Набойщиков.Алексей <.@TERM1>
2025-03-24 11:33:20 +0300

    добавили прогресс бар и иконки, начало спвм

--- AppData/TypingService.cs
+++ AppData/TypingService.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -7,6 +8,15 @@ namespace FastType.AppData
 {
     public class TypingService
     {
+        // Создаем поля для хранения  и использования переменных для внунтренней логики класса
+
+        private Stopwatch _stopwatch;
+        private int _numberOfCharacters;
+        private int _ellapsedTimeinMinutes;
+
+        // Создаем свойство для ПОЛУЧЕНИЯ И ПРИСВАЕВАНИЯ  расчетов 
+        public int TypeSpeed { get; private set; }
+
         // Создаем поля для хранения и использования в рамках класса
         private Grid _keyboardDrid;
         private TextBox _typingTextBox;
@@ -18,8 +28,18 @@ namespace FastType.AppData
             _keyboardDrid = keyboardDrid;
             _typingTextBox = typingTextBox;
             _typingTextBlock = typingTextBlock;
+
             _typingTextBox.PreviewKeyDown += _typingTextBox_PreviewKeyDown;
             _typingTextBox.PreviewKeyUp += _typingTextBox_PreviewKeyUp;
+            _typingTextBox.TextChanged += _typingTextBox_TextChanged;
+        }
+
+        private void _typingTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_typingTextBox.Text.Length >= 1 && !_stopwatch.IsRunning)
+            {
+
+            }
         }
 
         private void _typingTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
